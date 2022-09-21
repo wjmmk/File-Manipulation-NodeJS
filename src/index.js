d8bc4038a121ebf74cf773c9f24dc41877813047
William Mosquera <wjmmk3@gmail.com>
2022-09-21 16:28:39 -0500

    REFACTOR

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const PATH_FILE = path.join(__dirname, '../static_file/Address.csv')
+const PATH_FILE = path.join(__dirname, '../static_files/Address.csv')
 
 const mainFunction = () => {
     fs.readFile(PATH_FILE, 'utf8', (err, data) => {
