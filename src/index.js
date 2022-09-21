074b4b2318b8cf83eb1f344a2a85b3732885389e
William Mosquera <wjmmk3@gmail.com>
2022-09-21 16:23:55 -0500

    Initial Load

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+
+const PATH_FILE = path.join(__dirname, '../Address.csv')
+
+const mainFunction = () => {
+    fs.readFile(PATH_FILE, 'utf8', (err, data) => {
+        if(!err){
+            console.log(data)
+        }
+    })
+}
+
+mainFunction();
