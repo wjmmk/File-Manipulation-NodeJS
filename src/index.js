d3dca8f5589a13fe1a26a5f95b9789ccfee71f9e
William Mosquera <wjmmk3@gmail.com>
2022-09-23 16:26:44 -0500

    DockerFiles Added

--- src/index.js
+++ src/index.js
@@ -4,9 +4,16 @@ const path = require('path');
 const PATH_FILE = path.join(__dirname, '../static_files/Address.csv')
 
 const mainFunction = () => {
+    let count = 0;
     fs.readFile(PATH_FILE, 'utf8', (err, data) => {
         if(!err){
-            console.log(data)
+            const arrayLines = data.split('\n');
+            for( const registro in arrayLines ) {
+                const result = arrayLines[registro];
+                //console.log(result);
+               if(result.includes('La Paz')) count += 1;
+            }
+            console.log(`Esta es la cantidad de veces que aparece la palabra: Torrance `, count);
         }
     })
 }
