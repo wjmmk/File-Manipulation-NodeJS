c24ffc462bdb38606220b03edf2705fc190db543
William Mosquera <wjmmk3@gmail.com>
2022-09-26 15:48:38 -0500

    test finished

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
-const fs = require('fs');
-const path = require('path');
-const { on } = require('stream');
+const queue = require('bull');
 
-const PATH_FILE = path.join(__dirname, '../static_files/Person.csv')
+const fs = require('fs');
+const eStream = require('event-stream');
+const path = require('path');
+
+const lineQueue = new queue('Line_Queue', 'redis://redis:6380');
+
+    lineQueue.process( (job, done) => {
+        setTimeout(() => {
+            const { data } = job;
+            console.log(data);
+            done();
+        }, 2000)
+    })
+
+const PATH_FILE = path.join(__dirname, '../static_files/Customer.csv')
 
 const mainFunction = () => {
     
     fs.createReadStream(PATH_FILE, "utf-8")
+    .pipe(eStream.split()) // Separa la data del archivo x salto de Linea
+    .pipe(eStream.map(function (line, cb) {
+        //do something with the line 
+        cb(null, line)
+      }))
     .on("data", (data) => {
-        console.log(Buffer.byteLength(data));
+       // console.log(Buffer.byteLength(data));
+       lineQueue.add({data}, {
+         attempts:1
+       })
     })
 
 }
